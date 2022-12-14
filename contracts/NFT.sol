578f18c65d2f9ad3b688e6a3f8c442a48c83cd69
Luong Duc Canh <canhld1306@gmail.com>
2022-12-14 15:18:20 +0700

    write smart contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -9,7 +9,7 @@ import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
 
 contract NFT is ERC721URIStorage {
-    using Counters for Counters.Counters;
+    using Counters for Counters.Counter;
     Counters.counter private _tokenIds;
     // counters allow us to keep track of tokenId
 
