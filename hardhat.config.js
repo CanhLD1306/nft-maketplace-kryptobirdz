e55d2647f10c4cb3b55e8e2b4c10c52629b68b60
Luong Duc Canh <canhld1306@gmail.com>
2022-12-14 09:19:44 +0700

    config Hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle")
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
-  solidity: "0.8.17",
+  defaultNetworkl: 'hardhat',
+  network: {
+    hardhat: {
+      chainId: 1337
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      account: []
+    }
+  },
+
+
+  solidity: {
+    version: "0.8.17",
+    setting: {
+      optimized: {
+        enable: true,
+        runs: 200
+      }
+    }
+  }
 };
