bf3426e7dff1e9b1cdf7b96bea9d493578a55278
Luong Duc Canh <canhld1306@gmail.com>
2022-12-16 16:14:03 +0700

    deploy smart contract, Buy NFTs function

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,8 +1,7 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-
-// bring in the openzeppelin ERC721 NFT functionality
+// we will bring in the openzeppelin ERC721 NFT functionality 
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
@@ -10,30 +9,29 @@ import '@openzeppelin/contracts/utils/Counters.sol';
 
 contract NFT is ERC721URIStorage {
     using Counters for Counters.Counter;
-    Counters.counter private _tokenIds;
-    // counters allow us to keep track of tokenId
+    Counters.Counter private _tokenIds;
+    // counters allow us to keep track of tokenIds
 
     // address of marketplace for NFTs to interact
     address contractAddress;
 
-
     // OBJ: give the NFT market the ability to transact with tokens or change ownership
-    // setApprovalForAll allows  us to do that with contract address
+    // setApprovalForAll allows us to do that with contract address 
 
     // constructor set up our address
-    constructor(address marketplaceAddress) ERD721('KryptoBirdz', 'KBIRDZ'){
+    constructor(address marketplaceAddress) ERC721('KryptoBirdz', 'KBIRDZ') {
         contractAddress = marketplaceAddress;
     }
 
-    function mintToken(string memory tokenURI) public returns(unit) {
+    function mintToken(string memory tokenURI) public returns(uint) {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId);
         // set the token URI: id and url
         _setTokenURI(newItemId, tokenURI);
-        // give the marketplace the approveal to transact between users
+        // give the marketplace the approval to transact between users
         setApprovalForAll(contractAddress, true);
-        // min the token and set it for sale - return id to do so
+        // mint the token and set it for sale - return the id to do so 
         return newItemId;
     }
 }
