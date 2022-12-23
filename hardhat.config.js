4e7141841b4b7424fc8cca9526356ee9837d20e3
Luong Duc Canh <canhld1306@gmail.com>
2022-12-23 11:13:27 +0700

    Front end form

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,6 @@
-require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-waffle")
 
-const projectId = 'b5a89d8b31fc4a0a81bb553b709d81f3'
+// const projectId = 'b5a89d8b31fc4a0a81bb553b709d81f3'
 const fs = require('fs');
 const keyData = fs.readFileSync('./p-key.txt', {
   encoding: 'utf8', flag: 'r'
@@ -11,16 +10,16 @@ module.exports = {
   defaultNetworkl: 'hardhat',
   network: {
     hardhat: {
-      chainId: 1337
+      chainId: 31337
     },
     localhost: {
-      url: `http://127.0.0.1:8545`,
-      account: [keyData]
-    },
-    testnet: {
-      url: `https://near-testnet.infura.io/v3/${projectId}`,
+      url: `http://localhost:8545`,
       account: [keyData]
     }
+    // testnet: {
+    //   url: `https://aurora-testnet.infura.io/v3/${projectId}`,
+    //   account: [keyData]
+    // }
   },
 
 
--- pages/mint-item.js
+++ pages/mint-item.js
@@ -1,58 +1,60 @@
 import {ethers} from 'ethers'
 import {useState} from 'react'
 import Web3Modal from 'web3modal'
-import {create as ipfsHttpClient} from 'ipfs-http-client'
+import {create} from 'ipfs-http-client'
 import { nftaddress, nftmarketaddress } from '../config'
 import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
 import {useRouter} from 'next/router'
 import KBMarket from '../artifacts/contracts/KBMarket.sol/KBMarket.json'
 
+// in this component we set the ipfs up to host our nft data of
+// file storage 
 
-// in this component we set the ipfs  up to host our nft data of 
-// file storage
-
-const client =  ipfsHttpClient('http://127.0.0.1:8080')
+const client = create('http://localhost:5002/api/v0')
 
 export default function MintItem() {
     const [fileUrl, setFileUrl] = useState(null)
-    const [formInput, updateFormInput] = useState({price: '', name: '', description: ''})
+    const [formInput, updateFormInput] = useState({price: '', name:'',
+description:''})
     const router = useRouter()
 
-    // set up a function to fireoff when we update files in our form - we can add our
+
+    // set up a function to fireoff when we update files in our form - we can add our 
     // NFT images - IPFS
 
     async function onChange(e) {
         const file = e.target.files[0]
         try {
-            const added = await client.add(
-                file, {
-                    progress: (prog) => console.log(`received: ${prog}`)
-                }
-            )
-            const url = `http://127.0.0.1:8080/${added.path}`
-            setFileUrl(url)
+        const added = await client.add(
+            file, {
+                progress: (prog) => console.log(`received: ${prog}`)
+            })
+        const url = `https://ipfs.io/ipfs/${added.path}`
+        setFileUrl(url)
         } catch (error) {
-            console.log('Error uploading file: ', error)
-        } 
-
+            console.log('Error uploading file:', error)
+        }
     }
-    
+
     async function createMarket() {
-        const {name, description, price} = formInput
-        if(!name || !description || !price || !fileUrl) return
+        const {name, description, price} = formInput 
+        if(!name || !description || !price || !fileUrl) return 
         // upload to IPFS
         const data = JSON.stringify({
-            name, description, image: file
+            name, description, image: fileUrl
         })
         try {
             const added = await client.add(data)
-            const url = `${added.path}`
+            const url = `https://ipfs.io/ipfs/${added.path}`
+            // run a function that creates sale and passes in the url 
             createSale(url)
-        } catch (error) {
-            console.log('Error uploading file: ', error)
-        }
+            } catch (error) {
+                console.log('Error uploading file:', error)
+            }
     }
+
     async function createSale(url) {
+        // create the items and list them on the marketplace
         const web3Modal = new Web3Modal()
         const connection = await web3Modal.connect()
         const provider = new ethers.providers.Web3Provider(connection)
@@ -60,15 +62,14 @@ export default function MintItem() {
 
         // we want to create the token
         let contract = new ethers.Contract(nftaddress, NFT.abi, signer)
-        let transaction = await contract.createToken(url)
+        let transaction = await contract.mintToken(url)
         let tx = await transaction.wait()
         let event = tx.events[0]
         let value = event.args[2]
         let tokenId = value.toNumber()
         const price = ethers.utils.parseUnits(formInput.price, 'ether')
-
-        // list the item for sale on the marketplace
-
+        
+        // list the item for sale on the marketplace 
         contract = new ethers.Contract(nftmarketaddress, KBMarket.abi, signer)
         let listingPrice = await contract.getListingPrice()
         listingPrice = listingPrice.toString()
@@ -77,6 +78,7 @@ export default function MintItem() {
         await transaction.wait()
         router.push('./')
     }
+
     return (
         <div className='flex justify-center'>
             <div className='w-1/2 flex flex-col pb-12'>
@@ -112,4 +114,5 @@ export default function MintItem() {
             </div>
         </div>
     )
+
 }
